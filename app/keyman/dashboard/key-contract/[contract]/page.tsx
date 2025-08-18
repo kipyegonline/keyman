06003e4c5dc36155767132a6f30f837d261d350e
Vincent Kipyegon <Vinnykipx@gmail.com>
2025-08-18 16:23:47 +0300

    replce local storage

--- app/keyman/dashboard/key-contract/[contract]/page.tsx
+++ app/keyman/dashboard/key-contract/[contract]/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function page({ params }) {
+export default function page() {
   return (
     <div>
-      <h1>Contract ID: {params.contract}</h1>
+      <h1>Contract ID: </h1>
     </div>
   );
 }
